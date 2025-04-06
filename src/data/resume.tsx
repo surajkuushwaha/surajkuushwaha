f6f3d0612aac4b951330aa6f3aa6788b43af7788
Suraj Kushwaha <surajkuushwaha@gmail.com>
2025-04-06 11:30:01 +0000

    refactor: update avatar image and enhance resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -11,18 +11,21 @@ export const DATA = {
         "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
     summary:
         "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-    avatarUrl: "/me.png",
+    avatarUrl: "/me.jpg",
     skills: [
+        "Express",
+        "MySql",
+        "Sequelize",
+        "Node.js",
+        "MongoDB",
+        "Typescript",
         "React",
         "Next.js",
-        "Typescript",
-        "Node.js",
         "MySql",
         "Postgres",
         "Docker",
         "Kubernetes",
         "Java",
-        "C++",
     ],
     navbar: [
         { href: "/", icon: HomeIcon, label: "Home" },
@@ -41,24 +44,24 @@ export const DATA = {
             },
             LinkedIn: {
                 name: "LinkedIn",
-                url: "https://dub.sh/surajkuushwaha",
+                url: "https://www.linkedin.com/in/surajkuushwaha/",
                 icon: Icons.linkedin,
 
                 navbar: true,
             },
             X: {
                 name: "X",
-                url: "https://dub.sh/dillion-twitter",
+                url: "https://x.com/surajkuushwaha",
                 icon: Icons.x,
 
                 navbar: true,
             },
-            Youtube: {
-                name: "Youtube",
-                url: "https://dub.sh/dillion-youtube",
-                icon: Icons.youtube,
-                navbar: true,
-            },
+            // Youtube: {
+            //     name: "Youtube",
+            //     url: "https://dub.sh/dillion-youtube",
+            //     icon: Icons.youtube,
+            //     navbar: true,
+            // },
             email: {
                 name: "Send Email",
                 url: "#",
