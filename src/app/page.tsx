291a817c13ba132eab2be77799af6ae7d63c03e3
Suraj Kushwaha <skk74143@gmail.com>
2025-10-11 12:40:50 +0530

    feat: added exp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,24 +57,26 @@ export default function Page() {
                     <BlurFade delay={BLUR_FADE_DELAY * 5}>
                         <h2 className="text-xl font-bold">Work Experience</h2>
                     </BlurFade>
-                    {DATA.work.map((work, id) => (
-                        <BlurFade
-                            key={work.company}
-                            delay={BLUR_FADE_DELAY * 6 + id * 0.05}
-                        >
-                            <ResumeCard
-                                key={work.company}
-                                logoUrl={work.logoUrl}
-                                altText={work.company}
-                                title={work.company}
-                                subtitle={work.title}
-                                href={work.href}
-                                badges={work.badges}
-                                period={`${work.start} - ${work.end ?? "Present"}`}
-                                description={work.description}
-                            />
-                        </BlurFade>
-                    ))}
+                    {DATA.work.length > 0
+                        ? DATA.work.map((work, id) => (
+                              <BlurFade
+                                  key={work.company}
+                                  delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+                              >
+                                  <ResumeCard
+                                      key={work.company}
+                                      logoUrl={work.logoUrl}
+                                      altText={work.company}
+                                      title={work.company}
+                                      subtitle={work.title}
+                                      href={work.href}
+                                      badges={work.badges}
+                                      period={`${work.start} - ${work.end ?? "Present"}`}
+                                      description={work.description}
+                                  />
+                              </BlurFade>
+                          ))
+                        : null}
                 </div>
             </section>
             <section id="education">
