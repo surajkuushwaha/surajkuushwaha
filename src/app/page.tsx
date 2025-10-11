61629545dcd40fd6e1cec8a3a8418fd53d7ed796
Suraj Kushwaha <skk74143@gmail.com>
2025-10-12 03:33:14 +0530

    chore: update the contact content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -174,15 +174,14 @@ export default function Page() {
                                 Get in Touch
                             </h2>
                             <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                                Want to chat? Just shoot me a dm{" "}
+                                I&apos;m always open to discussing new opportunities, collaborations, or interesting projects. Drop me an{" "}
                                 <Link
-                                    href={DATA.contact.social.X.url}
+                                    href={`mailto:${DATA.contact.email}`}
                                     className="text-blue-500 hover:underline"
                                 >
-                                    with a direct question on twitter
+                                    email
                                 </Link>{" "}
-                                and I&apos;ll respond whenever I can. I will
-                                ignore all soliciting.
+                                and let&apos;s connect!
                             </p>
                         </div>
                     </BlurFade>
