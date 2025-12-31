06d8e9dd6fdd47dbef6422cc10a9f93310880ba1
Suraj Kushwaha <skk74143@gmail.com>
2025-12-31 21:55:05 +0530

    feat: add @vercel/speed-insights package and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const fontSans = FontSans({
@@ -67,6 +68,7 @@ export default function RootLayout({
                         <Navbar />
                     </TooltipProvider>
                 </ThemeProvider>
+                <SpeedInsights />
             </body>
         </html>
     );
